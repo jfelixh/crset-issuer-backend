8650fcfae3dd74883c88a3960381b642949772bd
Zexin Gong <zexin.gong@tum.de>
2025-01-20 19:33:31 +0100

    Add .env.example file and implement WebSocket server for event handling

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,35 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import statusRoutes from "./routes/statusRoutes";
+import {WebSocketServer, WebSocket} from "ws";
+import { EventEmitter } from "events";
 
 dotenv.config({ path: ".env" });
 
 const app: Express = express();
 const port = process.env.PORT;
+
+export const emitter = new EventEmitter();
+const wss = new WebSocketServer({ port: 8091 });
+wss.on('connection', (ws: WebSocket) => {
+  // Client identifier passed through the WebSocket protocol
+  console.log('Client connected');
+
+  // Forward events from the EventEmitter to the correct WebSocket client
+  const handleEvent = (eventData: any) => {
+    ws.send(JSON.stringify(eventData));
+  };
+
+  // Attach listener to the EventEmitter
+  emitter.on('progress', handleEvent);
+
+  // Handle client disconnection
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    emitter.removeListener('progress', handleEvent);
+  });
+});
+
 
 app.use(
   cors({
--- src/services/statusService.ts
+++ src/services/statusService.ts
@@ -9,7 +9,10 @@ import {
 import { connectToDb } from "src/db/database";
 import { sendBlobTransaction } from "src/utils/blob";
 import { randomString } from "src/utils/random-string";
-import * as bfc from "../../../../../padded-bloom-filter-cascade/src";
+import * as bfc from "../../../padded-bloom-filter-cascade/src/index";
+import * as process from "node:process";
+import {EventEmitter} from "events";
+import {emitter} from "../index";
 dotenv.config({ path: "../../.env" });
 
 interface StatusEntry {
@@ -21,7 +24,10 @@ interface StatusEntry {
 // Creates a new revocation status entry to be added to a VC before it is signed by the issuer.
 export async function createStatusEntry(): Promise<StatusEntry | null> {
   try {
-    const db = connectToDb(process.env.DB_LOCATION!);
+    if (!process.env.DB_LOCATION) {
+      throw new Error("No address provided");
+    }
+    const db = connectToDb(process.env.DB_LOCATION);
     // Generates a unique ID for a new status entry
     const statusPublisher = new AccountId({
       chainId: "eip155:1",
@@ -71,20 +77,30 @@ export async function getStatusByIDWithDatabase(id: string): Promise<boolean> {
 }
 
 // Publish BFC
-export async function publishBFC() {
+export async function publishBFC(): Promise<{ success: boolean, filter: any }> {
   try {
-    const db = connectToDb(process.env.DB_LOCATION!);
-    const validSet = await getIdsByStatus(db, "Valid");
-    const invalidSet = await getIdsByStatus(db, "Invalid");
+    if (!process.env.DB_LOCATION) {
+      throw new Error("No db location provided or wrong dotenv config");
+    }
+    
+    emitter?.emit('progress', {step: 'queryDB', status: 'started'});
+    const db = connectToDb(process.env.DB_LOCATION);
+    const validSet = await getIdsByStatus(db, "valid");
+    const invalidSet = await getIdsByStatus(db, "invalid");
+    emitter?.emit('progress', {step: 'queryDB', status: 'completed', additionalMetrics: {validSetSize: validSet.size, invalidSetSize: invalidSet.size}});
 
     // Calculate optimal rHat: rHat >= validSet.size AND rHat >= invalidSet.size / 2 (see pseudo code)
     const rHat =
       validSet.size > invalidSet.size / 2 ? validSet.size : invalidSet.size / 2;
 
+    emitter?.emit('progress', {step: 'constructBFC', status: 'started'});
     const temp = bfc.constructBFC(validSet, invalidSet, rHat);
+    emitter?.emit('progress', {step: 'constructBFC', status: 'completed', additionalMetrics: {levelCount: temp.length}});
+    emitter?.emit('progress', {step: 'serializeBFC', status: 'started'});
     const serializedData = bfc.toDataHexString(temp);
+    emitter?.emit('progress', {step: 'serializeBFC', status: 'completed', additionalMetrics: {serializedDataSize: serializedData.length/2/1000}});
 
-    sendBlobTransaction(
+    return sendBlobTransaction(
       process.env.INFURA_API_KEY!,
       process.env.PRIVATE_KEY!,
       process.env.ADDRESS!,
@@ -99,5 +115,6 @@ export async function publishBFC() {
       });
   } catch (error) {
     console.error("Error querying the database:", error);
+    return {success: false, filter: []};
   }
 }
